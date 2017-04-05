b50f0083afbd10a8ca18d6fb9d1ca693ecf641ae
Timur Samkharadze <timur.samkharadze@gmail.com>
2017-04-05 14:47:08 +0200

    i1

--- src/file.ads
+++ src/file.ads
@@ -16,16 +16,7 @@ package File is
    end record;
 
    type File_Record_Acess is access all File_Record;
-   --with Dynamic_Predicate => File_Record.Path'Length > 0
-     --and then File_Record.Path'Length <= Path_Max
-     --and then File_Record.File_Name'Length > 0
-     --and then File_Record.File_Name'Length <= Name_Max;
-
---   procedure Parse_Dir(Path: String) with Pre => Path'Length > 0 and then Path'Length <= Path_Max;
-
-  -- procedure Store_Dir_Structure;
-
-   --procedure Restore_Dir_Structure;
+   subtype Tree_Key is Unbounded_String;
 
 
 end File;
--- src/main.adb
+++ src/main.adb
@@ -8,7 +8,7 @@ with Ada.Directories;       use Ada.Directories;
 with Ada.IO_Exceptions;
 
 with Binary_Search_Tree;
-with File;
+with File; use File;
 with Ada.Real_Time;
 
 with Ada.Strings.Fixed;
@@ -17,7 +17,7 @@ with Ada.Strings.Fixed.Less_Case_Insensitive;
 with Ada.Calendar;
 with Ada.Strings.Unbounded.Less_Case_Insensitive;
 with GNAT.Directory_Operations; use GNAT.Directory_Operations;
-
+with SQLite;
 procedure Main is
    -- work with command line args
    package CLI renames Ada.Command_Line;
@@ -39,7 +39,7 @@ procedure Main is
    type Arguments is (help, path);
 
    --temp
-   subtype Tree_Key is Unbounded_String;
+
 
    function Key_Of (File_Record : File.File_Record_Acess) return Tree_Key is
    begin
@@ -54,8 +54,8 @@ procedure Main is
    function Compare_Nodes_Less (Left, Right : Tree_Key) return Boolean is
    begin
       return Ada.Strings.Unbounded.Less_Case_Insensitive
-          (Left  => Left,
-           Right => Right);
+        (Left  => Left,
+         Right => Right);
    end Compare_Nodes_Less;
 
    package Search_Tree is new Binary_Search_Tree
@@ -69,6 +69,7 @@ procedure Main is
    begin
       TIO.Put_Line (To_String (Element.Path) & Dir_Separator &  To_String (Element.File_Name));
       TIO.New_Line;
+
    end Process_Tree_Element;
 
    procedure Traverse_Tree is new Search_Tree.Traverse (Process_Tree_Element);
@@ -79,26 +80,26 @@ procedure Main is
       -- function that process every single file
       --
       procedure Process_Item (Item : Directory_Entry_Type) with
-         Pre => Kind (Item) = Ordinary_File is
-         Digest    : String (1 .. 32);
+        Pre => Kind (Item) = Ordinary_File is
+         Digest_File_Content    : String (1 .. 32);
          New_Entry : File.File_Record_Acess;
       begin
-         MD5.Driver.Digest_File (Full_Name (Item), Digest);
-         TIO.Put_Line (Digest);
+         MD5.Driver.Digest_File (Full_Name (Item), Digest_File_Content);
+         TIO.Put_Line (Digest_File_Content);
          New_Entry :=
            new File.File_Record'
              (Path =>
                 To_Unbounded_String (Containing_Directory (Full_Name (Item))),
               File_Name =>
                 To_Unbounded_String (Ada.Directories.Simple_Name (Item)),
-              Md_5_Hash => Digest,
+              Md_5_Hash => Digest_File_Content,
               Timestamp => Ada.Calendar.Clock);
          Search_Tree.Insert (Tree_Root, New_Entry);
          TIO.Put_Line (Search_Tree.Size (Tree => Tree_Root)'Img);
 
       end Process_Item;
       procedure Walk (Item : Directory_Entry_Type) with
-         Pre => Kind (Item) = Directory is
+        Pre => Kind (Item) = Directory is
       begin
          if Simple_Name (Item) /= "." and then Simple_Name (Item) /= ".." then
             Walk (Full_Name (Item), Pattern);
@@ -130,7 +131,7 @@ begin
          end if;
 
          Path_To_Search_U := To_Unbounded_String (CLI.Argument (2));
-      -- Parse_Dir(To_String(Path_To_Search_U));
+         -- Parse_Dir(To_String(Path_To_Search_U));
       when others =>
          raise Assertion_Error; -- must be impossible to jump here
    end case;
@@ -141,10 +142,18 @@ begin
    Elapsed_Time := ART."-" (Finish_Time, Start_Time);
    TIO.New_Line;
    TIO.Put_Line (ART.To_Duration (Elapsed_Time)'Img);
+   declare
+      Tmp : File.File_Record_Acess := Search_Tree.Retrieve(Tree => Tree_Root,
+                                                           Key  => To_Unbounded_String("C:\Users\80473\Dropbox\books\IT_books_and_papers\Ada\Ada-univercity\Ada 001\lectures-01_Overview-04_Ten_Bouncing_Balls\main.adb"));
+   begin
+      TIO.Put_Line(To_String(Tmp.File_Name));
+   end;
 
---  exception
---     when Constraint_Error =>
---        TIO.Put_Line ("You must provide at least one valid argument");
---        TIO.Put_Line ("Valid arguments are: help, path <path to search>");
---        return;
+
+
+   --  exception
+   --     when Constraint_Error =>
+   --        TIO.Put_Line ("You must provide at least one valid argument");
+   --        TIO.Put_Line ("Valid arguments are: help, path <path to search>");
+   --        return;
 end Main;
